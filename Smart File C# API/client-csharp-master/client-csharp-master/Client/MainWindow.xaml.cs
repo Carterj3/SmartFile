a001ed9dac8875b701d95f515d8e9b8ee79fab30
Jeffrey Carter <Carterj3@rose-hulman.edu>
2013-04-13 19:40:58 -0400

    More advanced client

--- Smart File C# API/client-csharp-master/client-csharp-master/Client/MainWindow.xaml.cs
+++ Smart File C# API/client-csharp-master/client-csharp-master/Client/MainWindow.xaml.cs
@@ -25,7 +25,123 @@ namespace Client
             InitializeComponent();
         }
 
-        private void Ribbon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void _Connect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewDirectory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rename_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Move_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Upload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Details_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void _AddUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _EditUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _RemoveUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _EditGroup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _RemoveGroup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditGroup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveGroup_Click(object sender, RoutedEventArgs e)
         {
 
         }
